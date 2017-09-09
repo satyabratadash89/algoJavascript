fe72aa21bc31ed3b34359552496fc82bb7b4f0b4
satyabratadash89 <satyabrata.dash.89@gmail.com>
2017-09-09 09:22:09 +0530

    Create ComplementaryDNA.js

---
+++ ComplementaryDNA.js
@@ -0,0 +1,23 @@
+/* 
+Description:
+
+Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+
+If you want to know more http://en.wikipedia.org/wiki/DNA
+
+In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have function with one side of the DNA (string, except for Haskell); you need to get the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+DNA_strand ("ATTGC") # return "TAACG"
+
+DNA_strand ("GTAT") # return "CATA"
+*/
+
+function DNAStrand(dna){
+  dna=dna.replace(/A/g,'X');
+  dna=dna.replace(/C/g,'Y');
+  dna=dna.replace(/T/g,'A');
+  dna=dna.replace(/G/g,'C');
+  dna=dna.replace(/X/g,'T');
+  dna=dna.replace(/Y/g,'G');
+  return dna;
+}
